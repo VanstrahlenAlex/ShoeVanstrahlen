0a4dddef3e52e698ad8426f24d525816c704901b
Alexander Van strahlen <vanstrahlenalex@gmail.com>
2024-07-12 18:05:36 -0500

    12-07-2024

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,67 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { MoreHorizontal, PlusCircle, UserIcon } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 export default function ProductsRoute() {
   return (
-	<div>ProductsPage</div>
+	<>
+		<div className='flex items-center justify-end'>
+			<Button asChild className='flex items-center gap-x-2'>
+				<Link href={"/dashboard/products/create"}>
+					<PlusCircle className='h-3.5 w-3.5' />
+					<span>Add Product</span>
+				</Link>
+			</Button>
+		</div>
+		<Card className='mt-5'>
+			<CardHeader>
+				<CardTitle>Products</CardTitle>
+				<CardDescription>Manage your products and view their sales performance</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead >Image</TableHead>
+							<TableHead>Name</TableHead>
+							<TableHead>Status</TableHead>
+							<TableHead>Price</TableHead>
+							<TableHead>Date</TableHead>
+							<TableHead className='text-end'>Actions</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						<TableRow>
+							<TableCell><UserIcon className='h-16 w-16'/></TableCell>
+							<TableCell>Nike Air 1</TableCell>
+							<TableCell>Active</TableCell>
+							<TableCell>$299.00</TableCell>
+							<TableCell>15/07/2024</TableCell>
+							<TableCell className='text-end'>
+								<DropdownMenu>
+									<DropdownMenuTrigger asChild>
+										<Button size="icon" variant={"ghost"}>
+											<MoreHorizontal  className='h-4 w-4' />
+										</Button>
+									</DropdownMenuTrigger>
+									<DropdownMenuContent align='end'>
+										<DropdownMenuLabel>Actions</DropdownMenuLabel>
+										<DropdownMenuSeparator />
+										<DropdownMenuItem>Edit</DropdownMenuItem>
+										<DropdownMenuItem className='text-red-600'>Delete</DropdownMenuItem>
+									</DropdownMenuContent>
+								</DropdownMenu>
+							</TableCell>
+
+						</TableRow>
+					</TableBody>
+				</Table>
+			</CardContent>
+		</Card>
+	</>
   )
 }
