e4824eacd0e85c144cdf040feb0ce11863ab4bef
Alexander Van strahlen <vanstrahlenalex@gmail.com>
2024-07-15 22:11:03 -0500

    15-07-2024

---
+++ app/lib/categories.ts
@@ -0,0 +1,17 @@
+export const categories = [
+	{
+		id: 0,
+		title: 'Men',
+		name: 'men',
+	},
+	{
+		id: 1,
+		title: 'Women',
+		name: 'men',
+	},
+	{
+		id: 2,
+		title: 'Kids',
+		name: 'kids',
+	}
+]
