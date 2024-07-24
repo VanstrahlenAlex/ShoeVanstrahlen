8a0c12fb96c6a8c163051a44b1b52519acb3fa0b
Alexander Van strahlen <vanstrahlenalex@gmail.com>
2024-07-23 23:42:20 -0500

    23-07-2024

--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Hero } from '../components/storefront/Hero'
+import { CategoriesSelection } from '../components/storefront/CategorySelection'
+import FeaturedProducts from '../components/storefront/FeaturedProducts'
 
 export default function IndexPage() {
   return (
 	<div>
 		<Hero />
+		<CategoriesSelection />
+		<FeaturedProducts />
 	</div>
   )
 }
